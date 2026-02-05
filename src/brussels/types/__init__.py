a0f20478a4fee14c5c4cfce8aed6186ec28ca4d9
Matthew LeMay <mplemay@users.noreply.github.com>
2026-02-05 13:45:54 -0600

    feat: add encrypted string type with optional extra (#13)

--- src/brussels/types/__init__.py
+++ src/brussels/types/__init__.py
@@ -2,3 +2,11 @@ from .datetime_utc import DateTimeUTC
 from .json_type import Json
 
 __all__ = ["DateTimeUTC", "Json"]
+
+try:
+    from .encrypted_string import EncryptedString
+except ModuleNotFoundError as exc:
+    if exc.name != "cryptography":
+        raise
+else:
+    __all__ += ["EncryptedString"]

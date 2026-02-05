b04045a30b5605dda398c00553d765cbdfeaecb5
Matt LeMay <mplemay@users.noreply.github.com>
2026-02-05 08:48:19 -0600

    feat: auto snake-case tablenames

--- src/brussels/base.py
+++ src/brussels/base.py
@@ -1,8 +1,9 @@
+import re
 from datetime import datetime
 from typing import Any, Final
 
 from sqlalchemy import MetaData
-from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
+from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, declared_attr
 
 from brussels.types import DateTimeUTC
 
@@ -18,11 +19,19 @@ TYPE_ANNOTATION_MAP: Final[dict[type | Any, object]] = {
     datetime: DateTimeUTC,
 }
 
+TABLENAME_CAPITAL_RUN_PATTERN: Final = re.compile(r"(?<=[A-Z]{2})([A-Z][a-z])")
+TABLENAME_BOUNDARY_PATTERN: Final = re.compile(r"(?<=[a-z0-9])([A-Z])")
+
 
 class Base(DeclarativeBase):
     __abstract__ = True
     metadata = MetaData(naming_convention=NAMING_CONVENTION)
     type_annotation_map = TYPE_ANNOTATION_MAP
+
+    @declared_attr.directive
+    def __tablename__(self) -> str:
+        name = TABLENAME_CAPITAL_RUN_PATTERN.sub(r"_\1", self.__name__)
+        return TABLENAME_BOUNDARY_PATTERN.sub(r"_\1", name).lower()
 
 
 class DataclassBase(MappedAsDataclass, Base):
